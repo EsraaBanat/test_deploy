6a8723344822a37db13d2033521bedb39475dd25
Esraa <esraa.bnt96@gmail.com>
2023-01-16 14:49:48 +0300

    add jenkins

--- components/Example.vue
+++ components/Example.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <h1>your code here</h1>
+    <h1>Enjoy with Jenkins</h1>
+    
+    <v-img
+  lazy-src="https://picsum.photos/id/11/10/6"
+  max-height="250"
+  max-width="250"
+  src="https://www.jenkins.io/images/logos/jenkins/Jenkins-stop-the-war.svg"
+></v-img>
   </div>
 </template>
 
